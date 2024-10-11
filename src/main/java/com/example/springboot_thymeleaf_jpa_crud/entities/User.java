5958233e737dde6ebbb59cfcf64f55d65643ebb8
NasirBhakshey <nasirbhakshey35@gmail.com>
2024-10-11 19:56:25 +0530

    first commit

--- src/main/java/com/example/springboot_thymeleaf_jpa_crud/entities/User.java
+++ src/main/java/com/example/springboot_thymeleaf_jpa_crud/entities/User.java
@@ -1,15 +1,11 @@
 package com.example.springboot_thymeleaf_jpa_crud.entities;
 
 import java.util.Date;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
